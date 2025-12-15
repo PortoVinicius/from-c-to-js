96efd8e8b20b76b6340c6739566bf6926a34496f
PortoVinicius <contato.viniciusporto@outlook.com>
2025-12-14 23:48:58 -0300

    teste.txt on step5

---
+++ c_code/hash/step5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Uso correto: ./step5 teste.txt");
+        return 1;
+    }
+    
+    // tenta abrir o arquivo
+    FILE *file = fopen("teste.txt", "r");
+
+    // Check se der erro
+    if (file == NULL) {
+        printf("Erro: Não foi possivel abrir o arquivo\n");
+        return 1;
+    }
+
+    // Sucesso
+    printf("Arquivo '%s' aberto com sucesso\n", argv[1]);
+
+    // Fecha o arquivo
+    fclose(file);
+    return 0;
+
+}
